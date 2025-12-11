7426a5b7a0f8a48fb395bca5a7ec818a37facc98
Lam Huynh <thanhlam0290@gmail.com>
2025-12-11 21:21:25 +0700

    update

--- app/main.py
+++ app/main.py
@@ -179,4 +179,4 @@ def download_acc(filename):
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
