4ac24a11d62cfeb03e44dcc2bd67b052207d72c2
Lam Huynh <thanhlam0290@gmail.com>
2025-12-14 09:06:01 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -18,8 +18,6 @@ from flask import Flask
 from flask_socketio import SocketIO
 from log_config import setup_logger
 
-logger = setup_logger("main")
-
 logger.info("Logger for main initialized")
 from downloader import download_video, get_video_info
 
