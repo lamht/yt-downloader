203df8c9ac34a90890383cd00a2d0dbe170d3a8e
Lam Huynh <thanhlam0290@gmail.com>
2025-12-17 08:15:04 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -211,4 +211,5 @@ def download_aac(filename):
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
